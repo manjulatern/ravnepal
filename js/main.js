c493e6c493a0837be932725c618ade91782c6821
neev89 <nirajanb89@gmail.com>
2016-09-18 18:54:49 +0545

    Chart height and cannot review theme css added

--- js/main.js
+++ js/main.js
@@ -28,4 +28,8 @@ $( function() {
     $( ".show-message" ).on( "click", function() {
         $(this).parents().next().toggleClass("is-visible");
     });
+
+    $( ".show-review-message" ).on( "click", function() {
+        $('.inline-message-review').toggleClass("is-visible");
+    });
 } );
