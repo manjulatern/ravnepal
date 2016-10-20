3623ddd15b1449dcadf78f92cdb1db48313da141
neev89 <nirajanb89@gmail.com>
2016-10-20 23:07:47 +0545

    Work on the Search filter on dxcode and cannot review section minor css fixed

--- js/main.js
+++ js/main.js
@@ -52,4 +52,38 @@ $( function() {
         $(this).closest('.chart-image').toggleClass('panel-fullscreen');
     });
 
+    var content = [
+        { title: 'Andorra' },
+        { title: 'United Arab Emirates' },
+        { title: 'Afghanistan' },
+        { title: 'Antigua' },
+        { title: 'Anguilla' },
+        { title: 'Albania' },
+        { title: 'Armenia' },
+        { title: 'Netherlands Antilles' },
+        { title: 'Angola' },
+        { title: 'Argentina' },
+        { title: 'American Samoa' },
+        { title: 'Austria' },
+        { title: 'Australia' },
+        { title: 'Aruba' },
+        { title: 'Aland Islands' },
+        { title: 'Azerbaijan' },
+        { title: 'Bosnia' },
+        { title: 'Barbados' },
+        { title: 'Bangladesh' },
+        { title: 'Belgium' },
+        { title: 'Burkina Faso' },
+        { title: 'Bulgaria' },
+        { title: 'Bahrain' },
+        { title: 'Burundi' }
+        // etc
+    ];
+
+    $('.add-claim-section .ui.search')
+        .search({
+            source: content
+        })
+    ;
+
 } );
